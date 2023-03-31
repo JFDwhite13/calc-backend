f95db83df819c5ad38bb9efb95cefd2b05d54a28
BuildTools <unconfigured@null.spigotmc.org>
2023-03-30 22:50:50 -0500

    v 0.0.4

--- index.js
+++ index.js
@@ -41,6 +41,27 @@ app.post(
 );
 
 app.post(
+    '/suma/multiple',
+    //se requieron dos objetos
+    // uno representando la peticion o datos de la peticion y otro representando la respuesta
+    (req, resp)=>{
+        //To do(por hacer): lo de aqui se ejecuta
+        console.log("Alguien se conecto a esta ruta"+" suma");
+        console.log(req.body);
+        const request = req.body
+        let suma = 0;
+        for(i in request){
+            var a = request[i];
+            console.log(a);
+            suma = suma + a;
+        }
+         console.log(suma);
+
+        resp.json("el resultado fue: "+suma)
+    }
+);
+
+app.post(
     '/resta',
     (req,resp)=>{
         const request = req.body
